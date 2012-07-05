99c31d3ab667c7542064e5dcce6d9b9c80e19dd0
Christopher H. Casebeer <christopher@chc.name>
2012-07-04 22:38:47 -0400

    Retab comments, fix indentation in docstring.

--- hkdf.py
+++ hkdf.py
@@ -5,54 +5,54 @@ http://tools.ietf.org/html/draft-krawczyk-hkdf-01
 
 Step 1: Extract
 
-   PRK = HKDF-Extract(salt, IKM)
+PRK = HKDF-Extract(salt, IKM)
 
-   Options:
-      Hash     a hash function; HashLen denotes the length of the
-               hash function output in octets
-   Inputs:
-      salt     optional salt value (a non-secret random value);
-               if not provided, it is set to a string of HashLen zeros.
-      IKM      input keying material
-   Output:
-      PRK      a pseudo-random key (of HashLen octets)
+Options:
+	Hash     a hash function; HashLen denotes the length of the
+				hash function output in octets
+Inputs:
+	salt     optional salt value (a non-secret random value);
+				if not provided, it is set to a string of HashLen zeros.
+	IKM      input keying material
+Output:
+	PRK      a pseudo-random key (of HashLen octets)
 
-   The output PRK is calculated as follows:
+The output PRK is calculated as follows:
 
-   PRK = HMAC-Hash(salt, IKM)
+PRK = HMAC-Hash(salt, IKM)
 
 Step 2: Expand
 
-   OKM = HKDF-Expand(PRK, info, L)
+OKM = HKDF-Expand(PRK, info, L)
 
-   Options:
-      Hash     a hash function; HashLen denotes the length of the
-               hash function output in octets
-   Inputs:
-      PRK      a pseudo-random key of at least HashLen octets
-               (usually, the output from the Extract step)
-      info     optional context and application specific information
-               (can be a zero-length string)
-      L        length of output keying material in octets
-               (<= 255*HashLen)
-   Output:
-      OKM      output keying material (of L octets)
+Options:
+	Hash     a hash function; HashLen denotes the length of the
+				hash function output in octets
+Inputs:
+	PRK      a pseudo-random key of at least HashLen octets
+				(usually, the output from the Extract step)
+	info     optional context and application specific information
+				(can be a zero-length string)
+	L        length of output keying material in octets
+				(<= 255*HashLen)
+Output:
+	OKM      output keying material (of L octets)
 
-   The output OKM is calculated as follows:
+The output OKM is calculated as follows:
 
-   N = ceil(L/HashLen)
-   T = T(1) | T(2) | T(3) | ... | T(N)
-   OKM = first L octets of T
+N = ceil(L/HashLen)
+T = T(1) | T(2) | T(3) | ... | T(N)
+OKM = first L octets of T
 
-   where:
-   T(0) = empty string (zero length)
-   T(1) = HMAC-Hash(PRK, T(0) | info | 0x01)
-   T(2) = HMAC-Hash(PRK, T(1) | info | 0x02)
-   T(3) = HMAC-Hash(PRK, T(2) | info | 0x03)
-   ...
+where:
+T(0) = empty string (zero length)
+T(1) = HMAC-Hash(PRK, T(0) | info | 0x01)
+T(2) = HMAC-Hash(PRK, T(1) | info | 0x02)
+T(3) = HMAC-Hash(PRK, T(2) | info | 0x03)
+...
 
-   (where the constant concatenated to the end of each T(n) is a
-   single octet.)
+(where the constant concatenated to the end of each T(n) is a
+single octet.)
 
 '''
 
@@ -62,8 +62,8 @@ import hashlib
 def hkdf_extract(salt, input_key_material, hash=hashlib.sha512):
 	'''
 	Extract a pseudorandom key suitable for use with hkdf_expand 
-   from the input_key_material and a salt using HMAC with the 
-   provided hash (default SHA-512). 
+	from the input_key_material and a salt using HMAC with the 
+	provided hash (default SHA-512). 
 
 	salt should be a random, application-specific byte string. If
 	salt is None or the empty string, an all-zeros string of the same
